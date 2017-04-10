a71257964af8a098c8f8a420384c9b41e2959898
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-10 00:19:56 -0700

    editing graph.c syntax

--- pa4/Graph.c
+++ pa4/Graph.c
@@ -52,7 +52,7 @@
     if(pG == NULL || (*pG) == NULL)
       return;
 
-    for (int i = 1;i <= getOrder(*pG); ++i) {
+    for (int i = 1; i <= getOrder(*pG); ++i) {
       freeList(&((*pG)->adjacent[i]));
     }
     free((*pG)->adjacent);
@@ -162,11 +162,11 @@
   // makeNull()
   // deletes all edges of G, restoring it to its original (no edge) state
   void makeNull(Graph G) { 
-    if(G == NULL) {
+    if (G == NULL) {
       printf("Graph Error: calling makeNull() on NULL Graph reference\n");
       exit(1);
     }
-    for(int i = 1; i <= getOrder(G); i++){
+    for (int i = 1; i <= getOrder(G); i++) {
       clear(G->adjacent[i]);
     }
     G->size = 0;
@@ -178,109 +178,108 @@
   // of v, and v to the adjacency List of u
   // pre: 1<=u<=getOrder(G)
   // pre: 1<=v<=getOrder(G)
-  void addEdge(Graph G, int u, int v){
+  void addEdge(Graph G, int u, int v) {
     if (G == NULL) {
       printf("Graph Error: calling addEdge() on NULL Graph reference\n");
       exit(1);
     }
-    if (u < 1 || u > getOrder(G) || v < 1 || v > getOrder(G)){
+    if (u < 1 || u > getOrder(G) || v < 1 || v > getOrder(G)) {
       printf("Graph Error: calling addEdge() with vertex out of bounds\n");
-   }
-   addArc(G,u,v);
-   addArc(G,v,u);
-   G->size++;   
-}
-// addArc()
-// inserts a new directed edge joining u to v, i.e. v is added to the 
-// adjacency List of u (but not u to the adjacency List of v)
-// pre: 1<=u<=getOrder(G)
-// pre: 1<=v<=getOrder(G)
-void addArc(Graph G, int u, int v){
-   if(G==NULL){
+    }
+    addArc(G, u, v);
+    addArc(G, v, u);
+    G->size++;   
+  }
+
+  // addArc()
+  // inserts a new directed edge joining u to v, i.e. v is added to the 
+  // adjacency List of u (but not u to the adjacency List of v)
+  // pre: 1<=u<=getOrder(G)
+  // pre: 1<=v<=getOrder(G)
+  void addArc(Graph G, int u, int v) {
+    if (G == NULL) {
       printf("Graph Error: calling addArc() on NULL Graph reference\n");
       exit(1);
-   }
-   if (u < 1 || u > getOrder(G) || v < 1 || v > getOrder(G)){
+    }
+    if (u < 1 || u > getOrder(G) || v < 1 || v > getOrder(G)) {
       printf("Graph Error: calling addArc() with vertex out of bounds\n");
       exit(1);
-   }
-  List adj = G->adjacent[u];
-  if(length(adj)==0){
-     append(adj,v);
-     return;
-  }
-  // move to front of G->adjacent[u]
-  moveFront(adj);
-  while(index(adj)!=-1){
-     if (v == get(adj)) //if v already exists in the list, return.
+    }
+    List adj = G->adjacent[u];
+    if (length(adj) == 0) {
+      append(adj, v);
+      return;
+    }
+    // move to front of G->adjacent[u]
+    moveFront(adj);
+    while (index(adj) != -1) {
+      if (v == get(adj)) //if v already exists in the list, return.
         return;
-     else if (v > get(adj)) //if v > current vertex in adj, move next
+      else if (v > get(adj)) //if v > current vertex in adj, move next
         moveNext(adj);
-      else{ // if v < current vertex in adj, insert before 
-        insertBefore(adj,v);
+      else { // if v < current vertex in adj, insert before 
+        insertBefore(adj, v);
         return;
-     }
-  } 
-   //if we have reached the end of the list
-   append(adj,v);
-   
+      }
+    }  
+    //if we have reached the end of the list
+    append(adj, v);
+  }  
 
-}
-
-// BFS()
-// runs the BFS algorithm on the Graph G with source s, setting the color, 
-// distance, parent, and source fields of G accordingly
-void BFS(Graph G, int s){
-   if(G==NULL){
+  // BFS()
+  // runs the BFS algorithm on the Graph G with source s, setting the color, 
+  // distance, parent, and source fields of G accordingly
+  void BFS(Graph G, int s) {
+    if (G == NULL) {
       printf("Graph Error: calling BFS() on NULL Graph reference\n");
       exit(1);
-   }
-   if (s < 1 || s > getOrder(G)){
+    }
+    if (s < 1 || s > getOrder(G)){
       printf("Graph Error: calling BFS() with vertex out of bounds\n");
       exit(1);
-   }
-   for(int i = 1; i <= getOrder(G); i++){
+    }
+    for (int i = 1; i <= getOrder(G); i++) {
       G->parent[i] = NIL;
       G->distance[i] = INF;
       G->color[i] = WHITE;
-   }
-   G->source = s;
-   G->color[s] = GRAY;
-   G->distance[s] = 0;
-   List Q = newList();
-   append(Q,s);
-   while(!isEmpty(Q)){
+    }
+    G->source = s;
+    G->color[s] = GRAY;
+    G->distance[s] = 0;
+    List Q = newList();
+    append(Q, s);
+    while (!isEmpty(Q)) {
       int u = front(Q);
       deleteFront(Q);
       List adjacent = G->adjacent[u];
       moveFront(adjacent);
-      while(index(adjacent) > -1){
-         int v = get(adjacent);
-         if(G->color[v] == WHITE){
-            G->color[v] = GRAY;
-            G->distance[v] = G->distance[u]+1;
-            G->parent[v] = u;
-            append(Q,v);
-         }
-         moveNext(adjacent);
+      while (index(adjacent) > -1) {
+        int v = get(adjacent);
+        if (G->color[v] == WHITE) {
+          G->color[v] = GRAY;
+          G->distance[v] = G->distance[u]+1;
+          G->parent[v] = u;
+          append(Q, v);
+        }
+        moveNext(adjacent);
       }
       G->color[u] = BLACK; 
-   }
-   freeList(&Q);
-}
+    }
+    freeList(&Q);
+  }
      
-/*** Other operations ***/
+  // Other operations --------------------------------------------------------
 
-// printGraph()
-// prints the adjacency list representation of G to the file pointed to by out
-void printGraph(FILE* out, Graph G){
-   if(G == NULL || out == NULL) {
+  // printGraph()
+  // prints the adjacency list representation of G to the file pointed to by out
+  void printGraph(FILE *out, Graph G) {
+    if (G == NULL || out == NULL) {
       printf("Graph Error: called printGraph() on NULL Graph reference");
       exit(1);
-   }
-   for(int i = 1; i <= getOrder(G); ++i) {
+    }
+    for (int i = 1; i <= getOrder(G); ++i) {
       fprintf(out, "%d: ", i);
       printList(out, G->adjacent[i]);
       fprintf(out, "\n");
-   }
-}
+    }
+  }
