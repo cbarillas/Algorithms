cce71761ae1ff62a9f2a230032cf4f6c26144701
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-12 01:04:17 -0700

    editing GraphTest syntax

--- pa5/GraphTest.c
+++ pa5/GraphTest.c
@@ -1,71 +1,74 @@
-//-----------------------------------------------------------------------------
-// GraphTest.c
-// Test client for the Graph ADT
-//-----------------------------------------------------------------------------
+/*
+ * Carlos Barillas - cbarilla@ucsc.edu
+ * CMPS101 - PA5 - GraphTest.c
+ *
+ * Test client for the Graph ADT
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include"List.h"
 #include"Graph.h"
 
 
-int main(int argc, char* argv[]){
-   int i, n=8;
-   List S = newList();
-   Graph G = newGraph(n);
-   Graph T=NULL;
+int main(int argc, char *argv[]) {
+  int i, n = 8;
+  List S = newList();
+  Graph G = newGraph(n);
+  Graph T = NULL;
 
-   for(i=1; i<=n; i++) append(S, i);
-   addArc(G, 1,2);
-   addArc(G, 2,3);
-   addArc(G, 2,5);
-   addArc(G, 2,6);
-   addArc(G, 3,4);
-   addArc(G, 3,7);
-   addArc(G, 4,3);
-   addArc(G, 4,8);
-   addArc(G, 5,1);
-   addArc(G, 5,6);
-   addArc(G, 6,7);
-   addArc(G, 7,6);
-   addArc(G, 7,8);
-   addArc(G, 8,8);
+  for (i = 1; i <=n ; i++) append(S, i);
+  addArc(G, 1, 2);
+  addArc(G, 2, 3);
+  addArc(G, 2, 5);
+  addArc(G, 2, 6);
+  addArc(G, 3, 4);
+  addArc(G, 3, 7);
+  addArc(G, 4, 3);
+  addArc(G, 4, 8);
+  addArc(G, 5, 1);
+  addArc(G, 5, 6);
+  addArc(G, 6, 7);
+  addArc(G, 7, 6);
+  addArc(G, 7, 8);
+  addArc(G, 8, 8);
 
-   printf("Graph G:\n");
-   printGraph(stdout, G);
+  printf("Graph G:\n");
+  printGraph(stdout, G);
 
-   printf("After running DFS on G\n");
-   DFS(G, S);
-   printf("\n");
-   printf("x:  d  f  p\n");
-   for(i=1; i<=n; i++){
-      fprintf(stdout, "%d: %2d %2d %2d\n",
-      i, getDiscover(G, i), getFinish(G, i), getParent(G, i));
-   }
+  printf("After running DFS on G\n");
+  DFS(G, S);
+  printf("\n");
+  printf("x:  d  f  p\n");
+  for (i = 1; i<= n; i++){
+    fprintf(stdout, "%d: %2d %2d %2d\n",
+    i, getDiscover(G, i), getFinish(G, i), getParent(G, i));
+  }
 
-   printf("Stack:\n");
-   printList(stdout, S);
-   printf("\n");
+  printf("Stack:\n");
+  printList(stdout, S);
+  printf("\n");
 
-   printf("Transpose of graph G:\n");
-   T = transpose(G);
-   printGraph(stdout, T);
-   printf("\n");
+  printf("Transpose of graph G:\n");
+  T = transpose(G);
+  printGraph(stdout, T);
+  printf("\n");
 
-   printf("After running DFS on T\n");
-   DFS(T, S);
-   printf("\n");
-   printf("x:  d  f  p\n");
-   for(i=1; i<=n; i++){
-      fprintf(stdout, "%d: %2d %2d %2d\n",
-      i, getDiscover(T, i), getFinish(T, i), getParent(T, i));
-   }
+  printf("After running DFS on T\n");
+  DFS(T, S);
+  printf("\n");
+  printf("x:  d  f  p\n");
+  for (i = 1; i <= n; i++){
+    fprintf(stdout, "%d: %2d %2d %2d\n",
+    i, getDiscover(T, i), getFinish(T, i), getParent(T, i));
+  }
 
-   printf("Stack: \n");
-   printList(stdout, S);
-   printf("\n");
-   freeList(&S);
-   freeGraph(&G);
-   freeGraph(&T);
-   return(0);
+  printf("Stack: \n");
+  printList(stdout, S);
+  printf("\n");
+  freeList(&S);
+  freeGraph(&G);
+  freeGraph(&T);
+  return(0);
 }
 
