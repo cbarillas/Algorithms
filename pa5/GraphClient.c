f6e786d85a75255eca187998c339e0c549ec1c0d
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-13 14:08:54 -0700

    editing GraphClient syntax

--- pa5/GraphClient.c
+++ pa5/GraphClient.c
@@ -18,19 +18,19 @@ int main(int argc, char *argv[]) {
 
   for(i = 1; i <= n; i++) append(S, i);
 
-  addArc(G, 1,2);
-  addArc(G, 1,5);
-  addArc(G, 2,5);
-  addArc(G, 2,6);
-  addArc(G, 3,2);
-  addArc(G, 3,4);
-  addArc(G, 3,6);
-  addArc(G, 3,7);
-  addArc(G, 3,8);
-  addArc(G, 6,5);
-  addArc(G, 6,7);
-  addArc(G, 8,4);
-  addArc(G, 8,7);
+  addArc(G, 1, 2);
+  addArc(G, 1, 5);
+  addArc(G, 2, 5);
+  addArc(G, 2, 6);
+  addArc(G, 3, 2);
+  addArc(G, 3, 4);
+  addArc(G, 3, 6);
+  addArc(G, 3, 7);
+  addArc(G, 3, 8);
+  addArc(G, 6, 5);
+  addArc(G, 6, 7);
+  addArc(G, 8, 4);
+  addArc(G, 8, 7);
   printGraph(stdout, G);
 
   DFS(G, S);
