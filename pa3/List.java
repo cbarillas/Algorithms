257d5484df7274f7f548d6d3ac313fe582804fa5
cbarilla@ucsc.edu <cbarilla@unix1.lt.ucsc.edu>
2017-04-14 00:55:20 -0700

    editing list syntax

--- pa3/List.java
+++ pa3/List.java
@@ -1,5 +1,4 @@
 /*
- *
  * Carlos Barillas - cbarilla@ucsc.edu
  * CMPS101 - PA3 - List.java 
  *
@@ -51,7 +50,7 @@ class List {
     index = -1;
   }
 
-  // Access functions -------------------------------------------------------
+  // Access functions --------------------------------------------------------
 
   // Returns the number of elements in this List
   int length() {
@@ -95,208 +94,210 @@ class List {
   public boolean equals(List L) {
     Node N = this.front;
     Node M = L.front;
-      if (this.length == L.length()) {
-        while (N != null && M!=null) {
-          if (N.data.equals(M.data)) {
-            N = N.next;
-            M = M.next;
-          }
-          else 
-           return false;
+    if (this.length == L.length()) {
+      while (N != null && M != null) {
+        if (N.data.equals(M.data)) {
+          N = N.next;
+          M = M.next;
         }
-        return true;
+        else 
+          return false;
       }
-      // L.length()!= length
-      return false;
+      return true;
+    }
+    // L.length()!= length
+    return false;
   }
    
-   // Manipulation Procedures -------------------------------------------------
+  // Manipulation Procedures -------------------------------------------------
    
-   // Resets this List to its original empty state.
-   void clear() {
+  // Resets this List to its original empty state.
+  void clear() {
     front = back = cursor = null;
     length = 0;
     index = -1;
-   }
+  }
 
-   // If List is non-empty, places the cursor under the front
-   // element, otherwise does nothing.
-   void moveFront() { 
-    if (length>0){ 
-     cursor = front;
-     index = 0;
+  // If List is non-empty, places the cursor under the front
+  // element, otherwise does nothing.
+  void moveFront() { 
+    if (length > 0) { 
+      cursor = front;
+      index = 0;
     }
-   }
-    // If List is non-empty, places the cursor under the back element,
-    // otherwise does nothing.
-   void moveBack() {
-    if (length>0) {
-     cursor = back;
-     index = length - 1;
+  }
+  
+  // If List is non-empty, places the cursor under the back element,
+  // otherwise does nothing.
+  void moveBack() {
+    if (length > 0) {
+      cursor = back;
+      index = length - 1;
     }
-   }
-    // If cursor is defined and not at front, moves cursor one step toward
-    // front of this List, if cursor is defined and at front, cursor becomes
-    // undefined, if cursor is undefined does nothing.
-   void movePrev(){
-    if (cursor != null && index!= 0){
-     cursor = cursor.prev;
-     index--;
+  }
+  
+  // If cursor is defined and not at front, moves cursor one step toward
+  // front of this List, if cursor is defined and at front, cursor becomes
+  // undefined, if cursor is undefined does nothing.
+  void movePrev(){
+    if (cursor != null && index != 0) {
+      cursor = cursor.prev;
+      index--;
     }
     else if (cursor != null && index == 0) {
-     cursor = null;
-     index = -1;
-    }
-   }
-    // If cursor is defined and not at back, moves cursor one step toward
-    // back of this List, if cursor is defined and at back, cursor becomes
-    // undefined, if cursor is undefined does nothing.
-   void moveNext() {
-    if (cursor != null && index != length-1){
-     cursor = cursor.next;
-     index++;
-    }
-    else if (cursor!=null && index == length-1){
-     cursor = null;
-     index = -1;
-    }
-   }
-    
-    // Insert new element into this List. If List is non-empty,
-    // insertion takes place before front element.
-   void prepend(Object data){
-    Node N;
-    if (front == null){
-     front = new Node(data);
-     back = front;
-     cursor = front;
-    }
-    else{
-     N = front;
-     N.prev = new Node(data);
-     front = N.prev;
-     front.next = N;
-    } 
-     length++;
-   }
-    
-    // Insert new element into this List. If List is non-empty,
-    // insertion takes place after back element.
-   void append(Object data){
-    Node N;
-    if (back == null){
-     back = new Node(data);
-     front = back;
-     cursor = back;
-    } 
-    else {
-     N = back;
-     N.next = new Node(data);
-     back = N.next;
-     back.prev = N;
-    }
-    length++;
-   }
-
-   // Inserts new element before cursor element in this
-   // List. Pre: length()>0, getIndex()>=0
-   void insertBefore(Object data) throws RuntimeException{
-    if(length<=0||index<0){
-     throw new RuntimeException( "List Error: insertBefore() called on an empty list.");
-    }
-    else{
-     if(index==0){
-     prepend(data);
-    }
-   else{
-     Node N = new Node(data);
-     Node before = cursor.prev;
-     N.prev = before;
-     N.next = cursor;
-     before.next = N;
-     cursor.prev= N;
-     length++;
-     index++;
-     }
-    }
-   }
-   
-   // Inserts new element after cursor.
-   // Pre: length()>0, index()>=0
-   void insertAfter(Object data){
-    if(length<=0||index<0){
-     throw new RuntimeException("List Error: insertAfter() called on empty list");
-   }
-   else {
-    if (index == length -1){
-     append(data);
-   }
-   else {
-     Node N = new Node(data);
-     Node after = cursor.next;
-     N.next = cursor.next;
-     N.prev = after;
-     after.prev = N;
-     cursor.next = N;
-     length++;
-     }
-    }
-   }
-			
-   // Deletes the front element. Pre: length()>0
-   void deleteFront() throws RuntimeException{
-    if (length<=0) 
-     throw new RuntimeException("List Error: deleteFront() called on empty list");
-    
-    if(cursor == front){
-     cursor = null;
-     index = -1;
-    }
-     front = front.next;
-     front.prev = null;
-     length--;
-    }
-
-    // Deletes the back element. Pre: length()>0
-    void deleteBack() throws RuntimeException{
-     if (length<=0)
-      throw new RuntimeException("List Error: deleteBack() called on empty list");
-     if(cursor == back) {
       cursor = null;
       index = -1;
-     }
-      back = back.prev;
-      back.next = null;
-      length--;
-     }
+    }
+  }
+  
+  // If cursor is defined and not at back, moves cursor one step toward
+  // back of this List, if cursor is defined and at back, cursor becomes
+  // undefined, if cursor is undefined does nothing.
+  void moveNext() {
+    if (cursor != null && index != length-1) {
+      cursor = cursor.next;
+      index++;
+    }
+    else if (cursor != null && index == length-1) {
+      cursor = null;
+      index = -1;
+    }
+  }
     
-    // Deletes cursor element, making cursor undefined.
-    // Pre: length()>0, index()>=0
-    void delete() throws RuntimeException{
-     if( length() <= 0 || index()< 0){
+  // Insert new element into this List. If List is non-empty,
+  // insertion takes place before front element.
+  void prepend(Object data) {
+    Node N;
+    if (front == null) {
+      front = new Node(data);
+      back = front;
+      cursor = front;
+    }
+    else{
+      N = front;
+      N.prev = new Node(data);
+      front = N.prev;
+      front.next = N;
+    } 
+    length++;
+  }
+    
+  // Insert new element into this List. If List is non-empty,
+  // insertion takes place after back element.
+  void append(Object data) {
+    Node N;
+    if (back == null) {
+      back = new Node(data);
+      front = back;
+      cursor = back;
+    } 
+    else {
+      N = back;
+      N.next = new Node(data);
+      back = N.next;
+      back.prev = N;
+    }
+    length++;
+  }
+
+  // Inserts new element before cursor element in this
+  // List. Pre: length()>0, getIndex()>=0
+  void insertBefore(Object data) throws RuntimeException {
+    if (length <= 0 || index < 0) {
+      throw new RuntimeException( "List Error: insertBefore() called on an empty list.");
+    }
+    else{
+      if(index == 0) {
+        prepend(data);
+      }
+      else {
+        Node N = new Node(data);
+        Node before = cursor.prev;
+        N.prev = before;
+        N.next = cursor;
+        before.next = N;
+        cursor.prev= N;
+        length++;
+        index++;
+      }
+    }
+  }
+   
+  // Inserts new element after cursor.
+  // Pre: length()>0, index()>=0
+  void insertAfter(Object data) {
+    if (length <= 0 || index < 0) {
+      throw new RuntimeException("List Error: insertAfter() called on empty list");
+    }
+    else {
+      if (index == length -1) {
+        append(data);
+      }
+      else {
+        Node N = new Node(data);
+        Node after = cursor.next;
+        N.next = cursor.next;
+        N.prev = after;
+        after.prev = N;
+        cursor.next = N;
+        length++;
+      }
+    }
+  }
+			
+  // Deletes the front element. Pre: length()>0
+  void deleteFront() throws RuntimeException {
+    if (length <= 0) 
+      throw new RuntimeException("List Error: deleteFront() called on empty list");
+    
+    if (cursor == front) {
+      cursor = null;
+      index = -1;
+    }
+    front = front.next;
+    front.prev = null;
+    length--;
+  }
+
+  // Deletes the back element. Pre: length()>0
+  void deleteBack() throws RuntimeException {
+    if (length <= 0)
+      throw new RuntimeException("List Error: deleteBack() called on empty list");
+    if (cursor == back) {
+      cursor = null;
+      index = -1;
+    }
+    back = back.prev;
+    back.next = null;
+    length--;
+  }
+    
+  // Deletes cursor element, making cursor undefined.
+  // Pre: length()>0, index()>=0
+  void delete() throws RuntimeException {
+    if( length() <= 0 || index() < 0) {
       throw new RuntimeException("List Error: delete() called on an empty list.");
-     }
-     else{
+    }
+    else{
       cursor.prev.next = cursor.next;
       cursor.next = cursor.prev;
       cursor = null;
       index = -1;
       length--;
-     }
     }
+  }
 
-   // Other Functions ---------------------------------------------------------
+  // Other Functions ---------------------------------------------------------
 
-   // toString()
-   // Overides Object's toString() method.
-   public String toString(){
-      String str = " ";
-      Node N = front;
-      while(N!=null){
-         str += N.toString() + " ";
-         N = N.next;
-      }
-      return str;
-   }
-
+  // toString()
+  // Overides Object's toString() method.
+  public String toString(){
+    String str = " ";
+    Node N = front;
+    while(N!=null){
+      str += N.toString() + " ";
+      N = N.next;
+    }
+    return str;
+  }
 }
